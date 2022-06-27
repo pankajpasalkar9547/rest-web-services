7a51ebe283a7c69188cce4b220ca7e35a286866c
Pankaj <pankajpasalkar9547@gmail.com>
2022-06-27 12:42:12 +0530

    Change in Return dev

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -10,7 +10,7 @@ public class HelloWorldController {
 	//@RequestMapping(method = RequestMathod.GET,path="/hello-world")
 	public String helloWorld()
 	{
-		return "Pankaj Pasalkar .";
+		return "Pankaj Pasalkar Dev.";
 	}
 	
 	@GetMapping(path="/hello-world-bean")
